524d6072bb943f64d66734d3005d1458bae49450
hafiz didarali <hafiz@Hafizs-MacBook-Pro.local>
2018-09-22 15:29:43 +0530

    added initial state to the application

---
+++ src/reducers/reducer_driver.js
@@ -0,0 +1,8 @@
+export default function () {
+  return [
+    { name: 'john hoe', title: 'the big man' },
+    { name: 'jenny dae', title: 'the serious lady' },
+    { name: 'hafiz ally', title: 'the owner' },
+    { name: 'alid didarali', title: 'works hard' },
+  ];
+}
